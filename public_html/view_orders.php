ef13bc3aae6a5b7cb8301a65bc0a1a81e7b4df59
Haoran <haleyyew@gmail.com>
2016-03-17 17:26:02 -0700

    initial commit

---
+++ public_html/view_orders.php
@@ -0,0 +1,115 @@
+<?php
+include 'functions.php';
+?>
+
+
+<p>Insert values into tab1 below:</p>
+<p><font size="2"> Number&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+Name</font></p>
+<form method="POST" action="oracle-test.php">
+<!--refresh page when submit-->
+
+   <p><input type="text" name="insNo" size="6"><input type="text" name="insName" 
+size="18">
+<!--define two variables to pass the value-->
+      
+<input type="submit" value="insert" name="insertsubmit"></p>
+</form>
+<!-- create a form to pass the values. See below for how to 
+get the values--> 
+
+<p> Update the name by inserting the old and new values below: </p>
+<p><font size="2"> Old Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+New Name</font></p>
+<form method="POST" action="oracle-test.php">
+<!--refresh page when submit-->
+
+   <p><input type="text" name="oldName" size="6"><input type="text" name="newName" 
+size="18">
+<!--define two variables to pass the value-->
+      
+<input type="submit" value="update" name="updatesubmit"></p>
+<input type="submit" value="run hardcoded queries" name="dostuff"></p>
+</form>
+
+<?php
+
+//this tells the system that it's no longer just parsing 
+//html; it's now parsing PHP
+
+$success = True; //keep track of errors so it redirects the page only if there are no errors
+$db_conn = OCILogon("ora_c4w9a", "a50851147", "ug");
+
+
+// Connect Oracle...
+if ($db_conn) {
+
+		if (array_key_exists('insertsubmit', $_POST)) {
+			//Getting the values from user and insert data into the table
+			$tuple = array (
+				":bind1" => $_POST['insNo'],
+				":bind2" => $_POST['insName']
+			);
+			$alltuples = array (
+				$tuple
+			);
+			executeBoundSQL("insert into tab1 values (:bind1, :bind2)", $alltuples);
+			OCICommit($db_conn);
+
+		} else
+			if (array_key_exists('updatesubmit', $_POST)) {
+				// Update tuple using data from user
+				$tuple = array (
+					":bind1" => $_POST['oldName'],
+					":bind2" => $_POST['newName']
+				);
+				$alltuples = array (
+					$tuple
+				);
+				executeBoundSQL("update tab1 set name=:bind2 where name=:bind1", $alltuples);
+				OCICommit($db_conn);
+
+			} else
+				if (array_key_exists('dostuff', $_POST)) {
+					// Insert data into table...
+					executePlainSQL("insert into tab1 values (10, 'Frank')");
+					// Inserting data into table using bound variables
+					$list1 = array (
+						":bind1" => 6,
+						":bind2" => "All"
+					);
+					$list2 = array (
+						":bind1" => 7,
+						":bind2" => "John"
+					);
+					$allrows = array (
+						$list1,
+						$list2
+					);
+					executeBoundSQL("insert into tab1 values (:bind1, :bind2)", $allrows); //the function takes a list of lists
+					// Update data...
+					//executePlainSQL("update tab1 set nid=10 where nid=2");
+					// Delete data...
+					//executePlainSQL("delete from tab1 where nid=1");
+					OCICommit($db_conn);
+				}
+
+	if ($_POST && $success) {
+		//POST-REDIRECT-GET -- See http://en.wikipedia.org/wiki/Post/Redirect/Get
+		header("location: oracle-test.php");
+	} else {
+		// Select data...
+		$result = executePlainSQL("select * from tab1");
+		printResult($result);
+	}
+
+	//Commit to save changes...
+	OCILogoff($db_conn);
+} else {
+	echo "cannot connect";
+	$e = OCI_Error(); // For OCILogon errors pass no handle
+	echo htmlentities($e['message']);
+}
+
+?>
+
